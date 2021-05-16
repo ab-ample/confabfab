b29c74feb7d97f7c0c7fa88d0f19db293b374858
ab-ample <38941353+ab-ample@users.noreply.github.com>
2021-05-16 11:29:35 +0200

    p5js drawing background

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+  
+function draw() {
+    if (mouseIsPressed) {
+        fill(230);
+        stroke(230);
+    } else {
+        fill(255);
+        stroke(230);
+    }
+        ellipse(mouseX, mouseY, 60, 60);
+}
