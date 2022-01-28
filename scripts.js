3843c24c3e20788af7b09be1c011ed2089bcd5d6
baalejandro <abample@gmail.com>
2022-01-28 21:39:21 +0200

    new index.html

--- scripts.js
+++ scripts.js
@@ -1,16 +1,1 @@
 console.log('🐓  🎀  𝒫𝒶𝓊𝓁𝒶, 𝒜𝓁𝑒𝒿𝒶𝓃𝒹𝓇☯, 𝐵𝒿ö𝓇𝓃  🎀  🐓');
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-}
-  
-function draw() {
-    if (mouseIsPressed) {
-        fill(230);
-        stroke(230);
-    } else {
-        fill(255);
-        stroke(230);
-    }
-        ellipse(mouseX, mouseY, 60, 60);
-}
