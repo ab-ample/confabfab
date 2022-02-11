4075f759131d9626d1d6c0a09a5fa7ab144710f7
baalejandro <abample@gmail.com>
2022-02-11 23:16:36 +0200

    new version at minimap

--- minimap/assets/js/main.js
+++ minimap/assets/js/main.js
@@ -1,22 +1,13 @@
-let websiteElement = document.getElementById('website');
-let minimapElement = document.getElementById('minimap');
-let innerMinimapElement = document.getElementById('inner-minimap');
+let captionTriggers = document.getElementsByClassName('captioned');
+let captionElement = document.getElementById('caption');
 
-function createMinimap() {
-	websiteElementClone = websiteElement.cloneNode(true);
-
-	innerMinimapElement.style.width = websiteElement.offsetWidth + 'px';
-	let minimapWidth = minimapElement.offsetWidth / websiteElement.offsetWidth;
-	
-	// translate(463px, 263px)
-	console.log(minimapWidth);
-	console.log(minimapElement.offsetWidth);
-	console.log(websiteElement.offsetWidth);
-
-	innerMinimapElement.style.transform = 'translate(0, ' + websiteElement.offsetWidth + 'px) scale(' + minimapWidth + ')';
-	innerMinimapElement.appendChild(websiteElementClone);
-
-	websiteElement.style.marginRight = '256px';
+for (let i = 0; i < captionTriggers.length; i++) {
+  let caption = captionTriggers[i].getAttribute('alt');
+  captionTriggers[i].addEventListener('mouseover', () => {displayCaption(caption)}, false);
+  captionTriggers[i].addEventListener('mouseout', () => {displayCaption('')}, false);
+  // captionTriggers[i].getAttribute('alt')
 }
 
-createMinimap();
+function displayCaption(caption) {
+  captionElement.innerHTML = caption;
+}
