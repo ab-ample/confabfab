a89483323334bb84fb7cee1a749509e6836588a3
baalejandro <abample@gmail.com>
2022-02-13 22:08:35 +0200

    new version arial

--- scroll/assets/js/main.js
+++ scroll/assets/js/main.js
@@ -15,7 +15,7 @@ function displayCaption(caption) {
 let backtomenuElement = document.getElementById("backtomenu");
 
 function scrollToShow() {
-  backtomenuElement.style.transform = "rotate("+(window.pageYOffset/4)+"deg)";
+  backtomenuElement.style.transform = "rotate("+(window.pageYOffset/8)+"deg)";
   let y = window.scrollY;
   if (y >= 800) {
     backtomenuElement.style.display = "block"
